37cca75002a84fa5615e462a9db08cdbbf93825e
Jade Dhangwattanotai <jd.dhang@gmail.com>
2022-06-26 13:23:10 +0700

    crawl shopee for price

--- main.js
+++ main.js
@@ -3,24 +3,39 @@
 // so that it can be started by running "npm start".
 
 const Apify = require('apify');
+const cheerio = require('cheerio');
 
 Apify.main(async () => {
-    // Get input of the actor.
-    // If you'd like to have your input checked and generate a user interface
-    // for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
-    const input = await Apify.getInput();
-    console.log('Input:');
-    console.dir(input);
+    console.log('Launching Puppeteer...');
+    const browser = await Apify.launchPuppeteer();
 
-    // Do something useful here...
+    const page = await browser.newPage();
 
-    // Save output
-    const output = {
-        receivedInput: input,
-        message: 'Hello sir!',
-    };
-    console.log('Output:');
-    console.dir(output);
-    await Apify.setValue('OUTPUT', output);
+    await page.goto('https://shopee.co.th/-%E0%B8%A1%E0%B8%B5-2-%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3-Royal-Canin-Kitten-Pouch-(12-Pouches)-%E0%B9%82%E0%B8%A3%E0%B8%A2%E0%B8%B1%E0%B8%A5%E0%B8%84%E0%B8%B2%E0%B8%99%E0%B8%B4%E0%B8%99-%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%9B%E0%B8%B5%E0%B8%A2%E0%B8%81%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%8B%E0%B8%AD%E0%B8%87-%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%A5%E0%B8%B9%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%A7%E0%B8%AD%E0%B8%B2%E0%B8%A2%E0%B8%B8-4-12%E0%B9%80%E0%B8%94%E0%B8%B7%E0%B8%AD%E0%B8%99-%E0%B8%9A%E0%B8%A3%E0%B8%A3%E0%B8%88%E0%B8%B8-12%E0%B8%8B%E0%B8%AD%E0%B8%87-i.68023848.1133684293')
+    
+    const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page()))); 
+    const popup = await newPagePromise;
+
+    const [button] = await page.$x("//button[contains(., 'English')]");
+    if (button) {
+        await button.click();
+    }
+
+
+    // ดึงราคาเป้าหมาย หรือส่วนที่ต้องการ
+    await page.waitForSelector('div[class = "pmmxKx"]');
+
+    const content = await page.content();
+
+    // นำผลลัพท์ที่ได้เข้า Cheerio io เพื่อให้เข้าถึงง่ายด้วยการเรียกใช้คล้ายๆ Jquery
+    const $ = cheerio.load(content);
+
+    const result = $('div[class = "pmmxKx"]');
+
+    console.log(result.text())
+
+    // Wait 30s to see Chromium works :)
+    await new Promise(resolve => setTimeout(resolve, 20000));
+
+    await browser.close();
 });
